779433445fcbf1b83bf435e071c4232a24ffb604
Gunnar Birke <gunnar.birke@online.de>
2021-05-14 23:21:23 +0200

    Add original runge kutta method

--- src/numode.py
+++ src/numode.py
@@ -25,3 +25,30 @@ def heun(y0, f, timesteps):
         y[i+1, :] = y[i, :] + 0.5 * deltat * (f(t, y[i, :]) + f(t + deltat, z))
         
     return y
+
+def runge_kutta(y0, f, timesteps):
+    # assume that we got more than one step, otherwise there will be no deltat
+    deltat = timesteps[1]
+    y = np.zeros((timesteps.size, y0.shape[0]))
+    y[0, :] = y0
+    
+    for i in range(0, timesteps.size - 1):
+        t = deltat * i
+        
+        tangent = 0.0
+        
+        eta1 = y[i, :]
+        tangent += (1.0 / 6.0) * f(t, eta1)
+        
+        eta2 = y[i, :] + deltat * 0.5 * f(t, eta1)
+        tangent += (1.0 / 3.0) * f(t + 0.5 * deltat, eta2)
+        
+        eta3 = y[i, :] + deltat * 0.5 * f(t + 0.5 * deltat, eta2)
+        tangent += (1.0 / 3.0) * f(t + 0.5 * deltat, eta3)
+        
+        eta4 = y[i, :] + deltat * f(t + 0.5 * deltat, eta3)
+        tangent += (1.0 / 6.0) * f(t + deltat, eta4)
+
+        y[i+1, :] = y[i, :] + deltat * tangent
+        
+    return y
