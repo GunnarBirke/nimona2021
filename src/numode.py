680866781470a36b65a4575dfd455053f7251f6b
Gunnar Birke <gunnar.birke@online.de>
2021-05-31 16:06:29 +0200

    Try to optimize RK method a bit

--- src/numode.py
+++ src/numode.py
@@ -29,26 +29,30 @@ def heun(y0, f, timesteps):
 def runge_kutta(y0, f, timesteps):
     # assume that we got more than one step, otherwise there will be no deltat
     deltat = timesteps[1]
-    y = np.zeros((timesteps.size, y0.shape[0]))
+    y = np.zeros((timesteps.size, y0.shape[0]), order='C')
     y[0, :] = y0
     
     for i in range(0, timesteps.size - 1):
         t = deltat * i
         
         tangent = 0.0
+        ycurr = y[i, :]
         
-        eta1 = y[i, :]
-        tangent += (1.0 / 6.0) * f(t, eta1)
+        eta1 = ycurr
+        f1 = f(t, eta1)
+        tangent += (1.0 / 6.0) * f1
         
-        eta2 = y[i, :] + deltat * 0.5 * f(t, eta1)
-        tangent += (1.0 / 3.0) * f(t + 0.5 * deltat, eta2)
+        eta2 = ycurr + deltat * 0.5 * f1
+        f2 = f(t + 0.5 * deltat, eta2)
+        tangent += (1.0 / 3.0) * f2
         
-        eta3 = y[i, :] + deltat * 0.5 * f(t + 0.5 * deltat, eta2)
-        tangent += (1.0 / 3.0) * f(t + 0.5 * deltat, eta3)
+        eta3 = ycurr + deltat * 0.5 * f2
+        f3 = f(t + 0.5 * deltat, eta3)
+        tangent += (1.0 / 3.0) * f3
         
-        eta4 = y[i, :] + deltat * f(t + 0.5 * deltat, eta3)
+        eta4 = ycurr + deltat * f3
         tangent += (1.0 / 6.0) * f(t + deltat, eta4)
 
-        y[i+1, :] = y[i, :] + deltat * tangent
+        y[i+1, :] = ycurr + deltat * tangent
         
     return y
