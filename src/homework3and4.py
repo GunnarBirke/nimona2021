0cc2c9b4524e9c15d396261ed94e1ca4c9e4b137
Gunnar Birke <gunnar.birke@online.de>
2021-06-14 15:58:05 +0200

    Update script for homework 3 and 4

--- src/homework3and4.py
+++ src/homework3and4.py
@@ -2,10 +2,7 @@ import numpy as np
 import numode as numode
 import network_model as network_model
 
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-
-import time
+import plotting as plot
 
 
 network = network_model.NetworkModel()
@@ -17,40 +14,13 @@ for i in range(network.adj.shape[0]):
         network.adj[i, j] = 50.0
         network.adj[j, i] = 50.0
         
-network.freqs = np.random.uniform(0.0, 2 * np.pi, 50)
+network.freqs = np.random.normal(loc=0.2, scale=0.01, size=50)
 y0 = np.random.uniform(0.0, 2 * np.pi, 50)
 
-start = time.time()
-print("hello")
-
 deltat = 0.001
-T = 150.0
+T = 1.5
 t = np.arange(0, T + deltat, deltat)
 y = numode.runge_kutta(y0, network, t)
 
-end = time.time()
-print("hello again")
-print(end - start)
-
-fig, ax = plt.subplots()
-ax.set_aspect('equal', 'box')
-circle = np.linspace(0, 2 * np.pi, 100)
-ax.plot(np.sin(circle), np.cos(circle))
-
-points, = ax.plot(np.sin(y0), np.cos(y0), '.r')
-
-def animate(i):
-    points.set_data(np.sin(y[i, :]), np.cos(y[i, :]))
-    return points,
-
-def init():
-    points.set_data(np.sin(y[0, :]), np.cos(y[0, :]))
-    return points,
-
-_ = animation.FuncAnimation(plt.gcf(), animate, t.shape[0], init_func=init,
-                            interval=200,
-                            blit=True, repeat_delay=1000)
-
-plt.title('Test')
-plt.show()
+plot.plotPhasesOnCircle(y, t, network.adj.shape[0])
     
