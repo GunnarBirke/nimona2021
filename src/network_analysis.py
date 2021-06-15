44b67f76d191cd334c09409f4f5a101a0e593af6
Gunnar Birke <gunnar.birke@online.de>
2021-06-15 17:28:22 +0200

    Load precalculated simulation into phase plot

--- src/network_analysis.py
+++ src/network_analysis.py
@@ -5,15 +5,12 @@ import plotting as plot
 
 network = network_model.NetworkModel()
 
-positions = np.load('./networks/northernPos.npy')
-network.adj = 50 * np.load('./networks/northernAdj.npy')
-
-network.freqs = np.random.normal(loc=0.2, scale=0.01, size=50)
-y0 = np.random.uniform(0.0, 2 * np.pi, 50)
+positions = np.load('./networks/ukPos.npy')
+network.adj = 50 * np.load('./networks/ukAdj.npy')
 
 deltat = 0.001
 T = 1.5
 t = np.arange(0, T + deltat, deltat)
-y = numode.runge_kutta(y0, network, t)
+y = np.load('./simulations/uk_random.npy')
 
-plot.plotPhasesOnCircle(y, t, network.adj.shape[0])
+plot.plotPhases(y, t, network.adj.shape[0], positions)
